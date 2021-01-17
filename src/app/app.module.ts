4f468e696a97f7059a9a1543a529ef88cb76a435
Alexis Dietl <alexisdietl@gmail.com>
2021-01-17 11:09:15 -0300

    Adding a header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { TripsComponent } from './trips/trips.component';
 import { TripsListComponent } from './trips/trips-list/trips-list.component';
 import { TripsDetailComponent } from './trips/trips-detail/trips-detail.component';
 import { TripsItemComponent } from './trips/trips-list/trips-item/trips-item.component';
 import { BackpackComponent } from './backpack/backpack.component';
 import { BackpackEditComponent } from './backpack/backpack-edit/backpack-edit.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
