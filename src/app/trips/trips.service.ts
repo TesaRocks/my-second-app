b03812083e2912c24f5570bdb516e0eda1a9e7fa
Alexis Dietl <alexisdietl@gmail.com>
2021-01-18 18:58:56 -0300

    adding a backpack Service

--- src/app/trips/trips.service.ts
+++ src/app/trips/trips.service.ts
@@ -6,13 +6,13 @@ export class TripsService {
   private trips: Trip[] = [
     new Trip(
       'Maui Hawaii',
-      'Great for Surf',
-      'https://upload.wikimedia.org/wikipedia/commons/4/49/Makena_Beach%2C_Maui_Hawaii_%2845015180584%29.jpg'
+      'Great for WindSurf',
+      'https://upload.wikimedia.org/wikipedia/commons/0/09/Hookipa_Maui_Wind_Surfer_James_Brennan_Molokai_Hawaii_-_panoramio.jpg'
     ),
     new Trip(
       'Matanzas Chile',
       'I love Windurfing',
-      'https://upload.wikimedia.org/wikipedia/commons/4/40/Sunset_Beach%2C_Oahu%2C_Hawaii%2C_USA5.jpg'
+      'https://as01.epimg.net/chile/imagenes/2018/10/04/deportes_aventura/1538661639_456641_1538661860_noticia_normal.jpg'
     ),
   ];
   getTrips() {
