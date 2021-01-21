e32b4884e39026e855950a2dfed3e0b3058d9919
Alexis Dietl <alexisdietl@gmail.com>
2021-01-21 15:15:24 -0300

    cleanup work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  //loeadeFeature = 'trip';
   in: number;
   sportSelected: string;
-  //onNavigate(feature: string) {
-  // this.loeadeFeature = feature;
-  //}
-  // toBB(event: number) {
-  //   this.in = event;
-  // }
-  // onSp(sp: string) {
-  //   this.sportSelected = sp;
-  // }
 }
