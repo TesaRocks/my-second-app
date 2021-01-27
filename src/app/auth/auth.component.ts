a5e97761babfadaa0b0a15451371504439b91c54
Alexis Dietl <alexisdietl@gmail.com>
2021-01-27 18:27:41 -0300

    adding authentication and loading spinner

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
+import { AuthService, AuthResponseData } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -10,25 +11,32 @@ export class AuthComponent {
   constructor(private authService: AuthService) {}
   isLogMode = true;
   isLoading = false;
+  error: string = null;
 
   onSubmit(form: NgForm) {
     if (!form.valid) {
       return;
     }
+    let obsAuth: Observable<AuthResponseData>;
     this.isLoading = true;
     if (this.isLogMode) {
+      obsAuth = this.authService.login(form.value.email, form.value.password);
     } else {
-      this.authService.signup(form.value.email, form.value.password).subscribe(
-        (resData) => {
-          console.log(resData);
-          this.isLoading = false;
-        },
-        (error) => {
-          console.log(error);
-          this.isLoading = false;
-        }
-      );
+      obsAuth = this.authService.signup(form.value.email, form.value.password);
     }
+    obsAuth.subscribe(
+      (resData) => {
+        console.log(resData);
+        this.isLoading = false;
+      },
+      (errorMessage: string) => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+
+        this.isLoading = false;
+      }
+    );
+
     form.reset();
   }
   onSwitchMode() {
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,22 +1,53 @@
-import { HttpClient } from '@angular/common/http';
-import { stringify } from '@angular/compiler/src/util';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  registered?: boolean;
 }
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   constructor(private http: HttpClient) {}
   signup(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD0xeXQwGRV2lySsceAyeIyofCA9u5XSOo',
-      { email: email, password: password, returnSecureToken: true }
-    );
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD0xeXQwGRV2lySsceAyeIyofCA9u5XSOo',
+        { email: email, password: password, returnSecureToken: true }
+      )
+      .pipe(catchError(this.handleError));
+  }
+  login(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD0xeXQwGRV2lySsceAyeIyofCA9u5XSOo',
+        { email: email, password: password, returnSecureToken: true }
+      )
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorM = 'An unknown error occurred';
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorM);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorM = 'This email already is in use';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorM = 'This email does not exist';
+        break;
+      case 'INVALID_PASSWORD':
+        errorM = 'Invalid Password';
+        break;
+    }
+    return throwError(errorM);
   }
 }
