d74f2f5d846aaef5f81a9870ba3f8bbd7c119eaf
Alexis Dietl <alexisdietl@gmail.com>
2021-02-05 09:55:46 -0300

    splitting modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { TripsComponent } from './trips/trips.component';
-import { TripsListComponent } from './trips/trips-list/trips-list.component';
-import { TripsDetailComponent } from './trips/trips-detail/trips-detail.component';
-import { TripsItemComponent } from './trips/trips-list/trips-item/trips-item.component';
-import { TripsStartComponent } from './trips/trips-start/trips-start.component';
 import { BackpackComponent } from './backpack/backpack.component';
 import { BackpackEditComponent } from './backpack/backpack-edit/backpack-edit.component';
 import { HeaderComponent } from './header/header.component';
@@ -16,10 +11,8 @@ import { BasicHighlightDirective } from './shared/basic-highlight.directive';
 import { BestHighlightDirective } from './shared/best-highlight.directive';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { DontClickDirective } from './shared/dontClick.directive';
-import { TripsEditComponent } from './trips/trips-edit/trips-edit.component';
 import { SurveyComponent } from './survey/survey.component';
 import { ShortenPipe } from './shared/shorten.pipe';
-import { TripRaComponent } from './trips/trips-start/trip-ra/trip-ra.component';
 import { CanDeactivateGuard } from './shared/can-deactivate-guard.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AuthComponent } from './auth/auth.component';
@@ -29,26 +22,20 @@ import { VipComponent } from './vip/vip.component';
 import { VipPageComponent } from './vip/vip-page/vip-page.component';
 import { Loading2SpinnerComponent } from './shared/loading-spinner/loading2-spinner.component';
 import { AlertComponent } from './shared/alert/alert.component';
+import { TripsModule } from './trips/trips.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    TripsComponent,
-    TripsListComponent,
-    TripsDetailComponent,
-    TripsItemComponent,
     BackpackComponent,
     BackpackEditComponent,
     BasicHighlightDirective,
     BestHighlightDirective,
     DropdownDirective,
     DontClickDirective,
-    TripsStartComponent,
-    TripsEditComponent,
     SurveyComponent,
     ShortenPipe,
-    TripRaComponent,
     PageNotFoundComponent,
     AuthComponent,
     LoadingSpinnerComponent,
@@ -63,6 +50,7 @@ import { AlertComponent } from './shared/alert/alert.component';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    TripsModule
   ],
   providers: [
     CanDeactivateGuard,
--- src/app/trips/trips.module.ts
+++ src/app/trips/trips.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { TripsComponent } from './trips.component';
+import { TripsListComponent } from './trips-list/trips-list.component';
+import { TripsDetailComponent } from './trips-detail/trips-detail.component';
+import { TripsItemComponent } from './trips-list/trips-item/trips-item.component';
+import { TripsStartComponent } from './trips-start/trips-start.component';
+import { TripsEditComponent } from './trips-edit/trips-edit.component';
+import { TripRaComponent } from './trips-start/trip-ra/trip-ra.component';
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-    declarations
+    declarations:[
+    TripsComponent,
+    TripsListComponent,
+    TripsDetailComponent,
+    TripsItemComponent,
+    TripsStartComponent,
+    TripsEditComponent,
+    TripRaComponent,
+
+    ],
+    imports:[RouterModule, CommonModule, ReactiveFormsModule],
+    exports:[
+        TripsComponent,
+    TripsListComponent,
+    TripsDetailComponent,
+    TripsItemComponent,
+    TripsStartComponent,
+    TripsEditComponent,
+    TripRaComponent,
+    ]
 })
 export class TripsModule{
 
